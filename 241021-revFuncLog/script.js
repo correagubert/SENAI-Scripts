acf66a7e622234af02221fa67587ec54ee3f56d2
correagubert <gubertnicolas@gmail.com>
2024-10-22 22:18:21 -0300

    Arquivos atualizados.

--- 241021-revFuncLog/script.js
+++ 241021-revFuncLog/script.js
@@ -2,38 +2,54 @@ let bancoDeDados = [];
 
 function add() {
     let username = document.getElementById("username").value;
-    let pos = bancoDeDados.indexOf(username.toUpperCase());
-    if (pos == -1) {
+    if (existe(username) == false) {
         bancoDeDados.push(username.toUpperCase());
         alert("Usuário cadastrado com sucesso.")
-        document.getElementById("username").value = null;
     } else {
         alert("Esse usuário já foi cadastrado.")
-        document.getElementById("username").value = null;
     }
-    // let existe = false;
-    // for (let item of bancoDeDados) {
-    //     if (item.toLowerCase() == username.toLowerCase()) {
-    //         existe = true;
-    //         break;
-    //     }
-    // }
-    //     if (existe == false) {
-    //         bancoDeDados.push(username.toUpperCase());
-    //         alert("Cadastro realizado com sucesso.")
-    //     }else{
-    //         alert("Este usuário já está cadastrado.")
-    //     }
+    document.getElementById("username").value = null;
 }
 
 function login(){
     let username = document.getElementById("user").value;
+    if (existe(username) == false) {
+        alert("Usuário não encontrado.")
+    } else {
+        alert("Bem vindo, "+username+".")
+    }
+    document.getElementById("user").value = null;
+}
+
+function edit(){
+    let username = document.getElementById("userEdit").value;
     let pos = bancoDeDados.indexOf(username.toUpperCase());
     if (pos == -1) {
         alert("Usuário não encontrado.")
-        document.getElementById("username").value = null;
     } else {
-        alert("Bem vindo, "+username+".")
-        document.getElementById("username").value = null;
+        let newUser = prompt("Insira o novo nome de usuário.")
+        bancoDeDados[pos] = newUser.toUpperCase()
+    }
+    document.getElementById("userEdit").value = null;
+}
+
+function remove(){
+    let username = document.getElementById("userDelete").value;
+    let pos = bancoDeDados.indexOf(username.toUpperCase());
+    if (pos == -1) {
+        alert("Usuário não encontrado.")
+    } else {
+        bancoDeDados.splice(pos, 1);
+        alert("Cadastro deletado com sucesso.")
+    }
+    document.getElementById("userDelete").value = null;
+}
+
+function existe(username){
+    let pos = bancoDeDados.indexOf(username.toUpperCase());
+    if (pos == -1) {
+        return false;
+    }else{
+        return true;
     }
 }
